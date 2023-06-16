1d23c71fe60a9754c4d27bc2d21c31f53fb389dd
SaanviPatnaik8711 <115956391+SaanviPatnaik8711@users.noreply.github.com>
2023-06-16 14:47:29 -0400

    Add files via upload

---
+++ ideachoice.js
@@ -0,0 +1,16 @@
+function openPage(){
+    window.location.replace("kwitter_room.html");
+}
+
+function openPage2(){
+    window.location.replace("kwitter_room2.html");
+}
+
+function openPage3(){
+    window.location.replace("kwitter_room3.html");
+}
+
+function openPage4(){
+    window.location.replace("kwitter_room4.html");
+}
+
